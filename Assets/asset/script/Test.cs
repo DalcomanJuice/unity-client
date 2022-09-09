e69da94b2e478ca77cc27826fe6c6da7c194d30c
jiminKim <programmer0415@gmail.com>
2022-09-09 19:12:53 +0900

    Chore: Unity2D Init

---
+++ Assets/asset/script/Test.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        //이거 한국어 인식하나
+        Debug.Log("Hello Unity");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
